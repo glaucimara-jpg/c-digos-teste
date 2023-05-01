2a691e71ff8af496109cdd5095d29fe35e04cd32
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-05-01 18:54:34 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+color = "red"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", myMousedown);
+
+
+
+
+
+function myMousedown(e)
+{
+    
+    color = document.getElementById("color").value;
+    console.log(color);
+    
+    
+     mouseX = e.clientX - canvas.offsetLeft;
+     mouseY = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouseX + " ,Y =  " + mouseY);
+    circle(mouseX , mouseY);    
+}
+
+
+
+
+
+
+function circle(mouseX , mouseY)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouseX ,mouseY, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+
