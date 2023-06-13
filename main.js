990f082de1a5003b42ec62ccc7b3f23cb82f038b
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-06-13 14:07:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -35,3 +35,49 @@ function speak(){
   var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
   synth.speak(utterThis);
 }
+
+
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("resultEmotionName").innerHTML = results[0].label;
+    document.getElementById("resultEmotionName2").innerHTML = results[1].label;
+    prediction1 = results[0].label;
+    prediction2 = results[1].label;
+    speak();
+    if(results[0].label == "feliz")
+    {
+	    document.getElementById("updateEmoji").innerHTML = "&#128522;";
+    }
+    if(results[0].label == "triste")
+    {
+	    document.getElementById("updateEmoji").innerHTML = "&#128532;";
+    }
+    if(results[0].label == "irritado")
+    {
+	    document.getElementById("updateEmoji").innerHTML = "&#128548;";
+    }
+
+    if(results[1].label == "feliz")
+    {
+	    document.getElementById("updateEmoji2").innerHTML = "&#128522;";
+    }
+    if(results[1].label == "triste")
+    {
+	    document.getElementById("updateEmoji2").innerHTML = "&#128532;";
+    }
+    if(results[1].label == "irritado")
+    {
+	    document.getElementById("updateEmoji2").innerHTML = "&#128548;";
+    }
+  }
+}
