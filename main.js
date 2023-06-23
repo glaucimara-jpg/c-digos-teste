79ad4c12b64e7d3058023428aca6336d08218147
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-06-22 21:10:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-prediction1 = ""
-prediction2 = ""
 
-Webcam.set({
+  Webcam.set({
     width:350,
     height:300,
-    imageFormat : 'png',
-    pngQuality:90
+    image_format : 'png',
+    png_quality:90
   });
 
-camera = document.getElementById("camera");
+  camera = document.getElementById("camera");
 
-Webcam.attach('#camera');
+Webcam.attach( '#camera' );
 
       
 function takeSnapshot()
@@ -22,21 +20,12 @@ function takeSnapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/J2Ip11PDF/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dhqOhdXJD/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
   }
-  
-function speak(){
-  var synth = window.speechSynthesis;
-  speakData1 = "A primeira previsão é " + prediction1;
-  speakData2 = "E a segunda previsão é " + prediction2;
-  var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
-  synth.speak(utterThis);
-}
-
-
+      
   function check()
   {
     img = document.getElementById('captured_image');
@@ -49,35 +38,7 @@ function gotResult(error, results) {
     console.error(error);
   } else {
     console.log(results);
-    document.getElementById("resultEmotionName").innerHTML = results[0].label;
-    document.getElementById("resultEmotionName2").innerHTML = results[1].label;
-    prediction1 = results[0].label;
-    prediction2 = results[1].label;
-    speak();
-    if(results[0].label == "feliz")
-    {
-	    document.getElementById("updateEmoji").innerHTML = "&#128522;";
-    }
-    if(results[0].label == "triste")
-    {
-	    document.getElementById("updateEmoji").innerHTML = "&#128532;";
-    }
-    if(results[0].label == "irritado")
-    {
-	    document.getElementById("updateEmoji").innerHTML = "&#128548;";
-    }
-
-    if(results[1].label == "feliz")
-    {
-	    document.getElementById("updateEmoji2").innerHTML = "&#128522;";
-    }
-    if(results[1].label == "triste")
-    {
-	    document.getElementById("updateEmoji2").innerHTML = "&#128532;";
-    }
-    if(results[1].label == "irritado")
-    {
-	    document.getElementById("updateEmoji2").innerHTML = "&#128548;";
-    }
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
   }
 }
