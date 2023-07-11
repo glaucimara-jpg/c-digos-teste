f2e3ad4b31d44f35919dfb99e98c36c36037098e
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-07-11 19:07:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-function modelLoaded() {
-  console.log('PoseNet foi inicializado');
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+  synth = window.speechSynthesis;
 }
 
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-15;
-    noseY = results[0].pose.nose.y-15;
-  }
+function preload() {
+
+
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+
+function clearCanvas() {
+
+  background("white");
 }
 
 function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clownNose, noseX, noseY, 30, 30);
+
+  // Set stroke weight to 13
+  strokeWeight(13);
+  // Set stroke color to black
+  stroke(0);
+  // If mouse is pressed, draw line between previous and current mouse positions
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
 }
 
-function takeSnapshot(){    
-  save('myFilterImage.png');
+function classifyCanvas() {
+  classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results);
+  document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+  document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+  utterThis = new SpeechSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
 }
