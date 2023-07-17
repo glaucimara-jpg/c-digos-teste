be1c30bd73b2b7a4d3b470ddee9ec4f5f18bf369
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-07-17 19:20:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,182 @@
-function setup() {
-  canvas = createCanvas(280, 280);
-  canvas.center();
-  background("white");
-  canvas.mouseReleased(classifyCanvas);
-  synth = window.speechSynthesis;
+
+var canvas = new fabric.Canvas('myCanvas');
+
+playerX = 10;
+playerY = 10;
+
+blockImageWidth = 30;
+blockImageHeight = 30;
+
+var playerObject= "";
+var blockImageObject= "";
+
+function playerUpdate()
+{
+	fabric.Image.fromURL("player.png", function(Img) {
+	playerObject = Img;
+
+	playerObject.scaleToWidth(150);
+	playerObject.scaleToHeight(140);
+	playerObject.set({
+	top:playerY,
+	left:playerX
+	});
+	canvas.add(playerObject);
+
+	});
 }
 
-function preload() {
+function newImage(getImage)
+{
+	fabric.Image.fromURL(getImage, function(Img) {
+	blockImageObject = Img;
 
+	blockImageObject.scaleToWidth(blockImageWidth);
+	blockImageObject.scaleToHeight(blockImageHeight);
+	blockImageObject.set({
+	top:playerY,
+	left:playerX
+	});
+	canvas.add(blockImageObject);
 
-  classifier = ml5.imageClassifier('DoodleNet');
+	});
+
 }
 
+window.addEventListener("keydown", myKeyDown);
 
-
-function clearCanvas() {
-
-  background("white");
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+if(e.shiftKey == true && keyPressed == '80')
+{
+	console.log("p e shift pressionadas juntas");
+	blockImageWidth = blockImageWidth + 10;
+	blockImageHeight = blockImageHeight + 10;
+	document.getElementById("currentWidth").innerHTML = blockImageWidth;
+	document.getElementById("currentHeight").innerHTML = blockImageHeight;	
+}
+if(e.shiftKey && keyPressed == '77')
+{
+	console.log("m e shift pressionadas juntas");
+	blockImageWidth = blockImageWidth - 10;
+	blockImageHeight = blockImageHeight - 10;
+	document.getElementById("currentWidth").innerHTML = blockImageWidth;
+	document.getElementById("currentHeight").innerHTML = blockImageHeight;
 }
 
-function draw() {
-
-  // Set stroke weight to 13
-  strokeWeight(13);
-  // Set stroke color to black
-  stroke(0);
-  // If mouse is pressed, draw line between previous and current mouse positions
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
+	if(keyPressed == '38')
+	{
+		up();
+		console.log("cima");
+	}
+	if(keyPressed == '40')
+	{
+		down();
+		console.log("baixo");
+	}
+	if(keyPressed == '37')
+	{
+		left();
+		console.log("esquerda");
+	}
+	if(keyPressed == '39')
+	{
+		right();
+		console.log("direita");
+	}
+	if(keyPressed == '87')
+	{
+		newImage('wall.jpg'); 
+		console.log("w");
+	}
+	if(keyPressed == '71')
+	{
+		newImage('ground.png'); 
+		console.log("g");
+	}
+	if(keyPressed == '76')
+	{
+		newImage('light_green.png'); 
+		console.log("l");
+	}
+	if(keyPressed == '84')
+	{
+		newImage('trunk.jpg'); 
+		console.log("t");
+	}
+	if(keyPressed == '82')
+	{
+		newImage('roof.jpg'); 
+		console.log("r");
+	}
+	if(keyPressed == '89')
+	{
+		newImage('yellow_wall.png'); 
+		console.log("y");
+	}
+	if(keyPressed == '68')
+	{
+		newImage('dark_green.png'); 
+		console.log("d");
+	}
+	if(keyPressed == '85')
+	{
+		newImage('unique.png'); 
+		console.log("u");
+	}
+	if(keyPressed == '67')
+	{
+		newImage('cloud.jpg'); 
+		console.log("c");
+	}
+	
+}
+function up()
+{
+	if(playerY >=0)
+	{
+		playerY = playerY - blockImageHeight;
+		console.log("autura da imagem do bloco = " + blockImageHeight);
+		console.log("Quando a tecla direcional Cima for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
 }
 
-function classifyCanvas() {
-  classifier.classify(canvas, gotResult);
+function down()
+{
+	if(playerY <=500)
+	{
+		playerY = playerY + blockImageHeight;
+		console.log("altura da imagem do bloco = " + blockImageHeight);
+		console.log("Quando a tecla direcional Baixo for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
 }
 
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(results);
-  document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+function left()
+{
+	if(playerX >0)
+	{
+		playerX = playerX - blockImageWidth;
+		console.log("largura da imagem do bloco = " + blockImageWidth);
+		console.log("Quando a tecla direcional Esquerda for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
+}
 
-  document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
-
-  utterThis = new SpeechSynthesisUtterance(results[0].label);
-  synth.speak(utterThis);
+function right()
+{
+	if(playerX <=850)
+	{
+		playerX = playerX + blockImageWidth;
+		console.log("largura da imagem do bloco = " + blockImageWidth);
+		console.log("Quando a tecla direcional Direita for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
 }
