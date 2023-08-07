bf15a884f48bf87991eaffb4cbe98a11100fd160
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-08-07 19:31:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,91 @@
-noseX=0;
-noseY=0;
+player1Name = localStorage.getItem("player1Name");
+	player2Name = localStorage.getItem("player2Name");
 
-function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+	player1Score = 0;
+	player2Score = 0;
+
+document.getElementById("player1Name").innerHTML = player1Name + " : ";
+document.getElementById("player2Name").innerHTML = player2Name + " : ";
+
+document.getElementById("player1Score").innerHTML = player1Score ;
+document.getElementById("player2Score").innerHTML = player2Score ;
+
+document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
+document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
+
+function send() {
+	getWord = document.getElementById("word").value;
+	word = getWord.toLowerCase();
+	console.log("palavra em caixa baixa = " + word);
+
+    charAt1 = word.charAt(1);
+	console.log(charAt1);
+
+	lenghtDivide2 = Math.floor(word.length/2);
+	charAt2 = word.charAt(lenghtDivide2);
+	console.log(charAt2);
+
+    lenghtMinus1 = word.length - 1; 
+    charAt3 = word.charAt(lenghtMinus1); 
+	console.log(charAt3);
+
+    removeCharAt1 = word.replace(charAt1, "_");
+    removeCharAt2 = removeCharAt1.replace(charAt2, "_");
+    removeCharAt3 = removeCharAt2.replace(charAt3, "_");
+	console.log(removeCharAt3);
+
+    questionWord = "<h4 id='wordDisplay'> P. "+removeCharAt3+"</h4>";
+    input_box = "<br>Resposta : <input type='text' id='inputCheckBox'>";
+    checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
+    row =  questionWord + input_box + checkButton ; 
+    document.getElementById("output").innerHTML = row;
+document.getElementById("word").value = "";
 }
 
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(300, 300);
-  video.hide();
 
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
+questionTurn = "player1";
+answerTurn = "player2";
 
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
-}
 
-function gotPoses(results)
+function check()
 {
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-15;
-    noseY = results[0].pose.nose.y-15;
-  }
-}
+	getAnswer = document.getElementById("inputCheckBox").value;
+	answer = getAnswer.toLowerCase();
+	console.log("resposta em caixa baixa - " + answer);
+	if(answer == word)	
+	{
+		if(answerTurn == "player1")
+		{
+			player1Score = player1Score +1;
+		    document.getElementById("player1Score").innerHTML = player1Score;
+		}
+		else 
+		{
+			player2Score = player2Score +1;
+		    document.getElementById("player2Score").innerHTML = player2Score;
+		}
+	}
+	if(questionTurn == "player1")
+	{
+		questionTurn = "player2"
+		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player2Name ;
+	}
+	else 
+	{
+		questionTurn = "player1"
+		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player1Name ;
+	}
 
-function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 30, 30);
-}
+	if(answerTurn == "player1")
+	{
+		answerTurn = "player2"
+		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player2Name ;
+	}
+	else 
+	{
+		answerTurn = "player1"
+		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player1Name ;
+	}
 
-function take_snapshot(){    
-  save('myFilterImage.png');
+    document.getElementById("output").innerHTML = "";
 }
