80fad95cf72ca6b7273239ee834c6abe1e5f7590
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-08-10 14:20:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,46 @@
-player1Name = localStorage.getItem("player1Name");
-	player2Name = localStorage.getItem("player2Name");
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-	player1Score = 0;
-	player2Score = 0;
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-document.getElementById("player1Name").innerHTML = player1Name + " : ";
-document.getElementById("player2Name").innerHTML = player2Name + " : ";
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
 
-document.getElementById("player1Score").innerHTML = player1Score ;
-document.getElementById("player2Score").innerHTML = player2Score ;
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
 
-document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
-document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
-
-function send() {
-	getWord = document.getElementById("word").value;
-	word = getWord.toLowerCase();
-	console.log("palavra em caixa baixa = " + word);
-
-    charAt1 = word.charAt(1);
-	console.log(charAt1);
-
-	lenghtDivide2 = Math.floor(word.length/2);
-	charAt2 = word.charAt(lenghtDivide2);
-	console.log(charAt2);
-
-    lenghtMinus1 = word.length - 1; 
-    charAt3 = word.charAt(lenghtMinus1); 
-	console.log(charAt3);
-
-    removeCharAt1 = word.replace(charAt1, "_");
-    removeCharAt2 = removeCharAt1.replace(charAt2, "_");
-    removeCharAt3 = removeCharAt2.replace(charAt3, "_");
-	console.log(removeCharAt3);
-
-    questionWord = "<h4 id='wordDisplay'> P. "+removeCharAt3+"</h4>";
-    input_box = "<br>Resposta : <input type='text' id='inputCheckBox'>";
-    checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
-    row =  questionWord + input_box + checkButton ; 
-    document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
 
 
-questionTurn = "player1";
-answerTurn = "player2";
-
-
-function check()
+function gotPoses(results)
 {
-	getAnswer = document.getElementById("inputCheckBox").value;
-	answer = getAnswer.toLowerCase();
-	console.log("resposta em caixa baixa - " + answer);
-	if(answer == word)	
-	{
-		if(answerTurn == "player1")
-		{
-			player1Score = player1Score +1;
-		    document.getElementById("player1Score").innerHTML = player1Score;
-		}
-		else 
-		{
-			player2Score = player2Score +1;
-		    document.getElementById("player2Score").innerHTML = player2Score;
-		}
-	}
-	if(questionTurn == "player1")
-	{
-		questionTurn = "player2"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player2Name ;
-	}
-	else 
-	{
-		questionTurn = "player1"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player1Name ;
-	}
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
 
-	if(answerTurn == "player1")
-	{
-		answerTurn = "player2"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player2Name ;
-	}
-	else 
-	{
-		answerTurn = "player1"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player1Name ;
-	}
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-    document.getElementById("output").innerHTML = "";
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
+
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Largura e altura serão = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
