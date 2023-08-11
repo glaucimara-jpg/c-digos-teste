cb22de93de718233ae669cd0f1c8f2c78e69ad96
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-08-11 18:59:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,25 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-  
-var recognition = new SpeechRecognition();
-
-var Textbox = document.getElementById("textbox"); 
-
-function start()
+function gotPoses(results)
 {
-    Textbox.innerHTML = ""; 
-    recognition.start();
-} 
- 
-recognition.onresult = function(event) {
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
 
- console.log(event); 
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-var Content = event.results[0][0].transcript;
-
-    Textbox.innerHTML = Content;
-    console.log(Content);
-      if(Content =="tire minha selfie")
-      {
-        console.log("tirando selfie --- ");
-        speak();
-      }
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
+function draw() {
+background('#969A97');
 
-function speak(){
-    var synth = window.speechSynthesis;
-
-    speakData = "Tirando sua selfie em 5 segundos";
-
-    var utterThis = new SpeechSynthesisUtterance(speakData);
-
-    synth.speak(utterThis);
-
-    Webcam.attach(camera);
-
-    setTimeout(function()
-    { 
-        takeSelfie(); 
-        save();
-    }
-    , 5000);
+  document.getElementById("square_side").innerHTML = "Largura e altura serão = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
 }
-
- 
-camera = document.getElementById("camera");
-Webcam.set({
-    width:360,
-    height:250,
-    image_format : 'jpeg',
-    jpeg_quality:90
-});
-
-
-
-
-function takeSelfie()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfieImage" src="'+data_uri+'"/>';
-    });
-}
-
-
-function save()
-{
-  link = document.getElementById("link");
-  image = document.getElementById("selfieImage").src ;
-  link.href = image;
-  link.click();
-}
-
