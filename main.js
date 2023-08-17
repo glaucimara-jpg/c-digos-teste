64702299eae387bf481b85124361a5152fb99ce1
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-08-17 14:11:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-function gotPoses(results)
+song = "";
+
+function preload()
 {
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX +" noseY = " + noseY);
-
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
-  }
+	song = loadSound("music.mp3");
 }
 
-function draw() {
-background('#969A97');
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
-  document.getElementById("square_side").innerHTML = "Largura e altura serão = " + difference +"px";
-  fill('#F90093');
-  stroke('#F90093');
-  square(noseX, noseY, difference);
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
