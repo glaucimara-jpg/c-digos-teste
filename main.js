8c053ff68f9043d35e77df5938b7359868a68b2c
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-08-22 14:23:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,83 @@
-layer1Name = localStorage.getItem("player1Name");
-	player2Name = localStorage.getItem("player2Name");
-
-	player1Score = 0;
-	player2Score = 0;
-
-document.getElementById("player1Name").innerHTML = player1Name + " : ";
-document.getElementById("player2Name").innerHTML = player2Name + " : ";
-
-document.getElementById("player1Score").innerHTML = player1Score ;
-document.getElementById("player2Score").innerHTML = player2Score ;
-
-document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
-document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
-
-function send() {
-	getWord = document.getElementById("word").value;
-	word = getWord.toLowerCase();
-	console.log("palavra em caixa baixa = " + word);
-
-    charAt1 = word.charAt(1);
-	console.log(charAt1);
-
-	lenghtDivide2 = Math.floor(word.length/2);
-	charAt2 = word.charAt(lenghtDivide2);
-	console.log(charAt2);
-
-    lenghtMinus1 = word.length - 1; 
-    charAt3 = word.charAt(lenghtMinus1); 
-	console.log(charAt3);
-
-    removeCharAt1 = word.replace(charAt1, "_");
-    removeCharAt2 = removeCharAt1.replace(charAt2, "_");
-    removeCharAt3 = removeCharAt2.replace(charAt3, "_");
-	console.log(removeCharAt3);
-
-    questionWord = "<h4 id='wordDisplay'> P. "+removeCharAt3+"</h4>";
-    input_box = "<br>Resposta : <input type='text' id='inputCheckBox'>";
-    checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
-    row =  questionWord + input_box + checkButton ; 
-    document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
-}
-
-
-questionTurn = "player1";
-answerTurn = "player2";
-
-
-function check()
+function gotPoses(results)
 {
-	getAnswer = document.getElementById("inputCheckBox").value;
-	answer = getAnswer.toLowerCase();
-	console.log("resposta em caixa baixa - " + answer);
-	if(answer == word)	
-	{
-		if(answerTurn == "player1")
-		{
-			player1Score = player1Score +1;
-		    document.getElementById("player1Score").innerHTML = player1Score;
-		}
-		else 
-		{
-			player2Score = player2Score +1;
-		    document.getElementById("player2Score").innerHTML = player2Score;
-		}
-	}
-	if(questionTurn == "player1")
-	{
-		questionTurn = "player2"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player2Name ;
-	}
-	else 
-	{
-		questionTurn = "player1"
-		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player1Name ;
-	}
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-	if(answerTurn == "player1")
-	{
-		answerTurn = "player2"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player2Name ;
-	}
-	else 
-	{
-		answerTurn = "player1"
-		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta: " + player1Name ;
-	}
-
-    document.getElementById("output").innerHTML = "";
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
+
+
+
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+		if(rightWristY >0 && rightWristY <= 100)
+		{
+			document.getElementById("speed").innerHTML = "Velocidade = 0.5x";		
+			song.rate(0.5);
+		}
+		else if(rightWristY >100 && rightWristY <= 200)
+		{
+			document.getElementById("speed").innerHTML = "Velocidade = 1x";		
+			song.rate(1);
+		}
+		else if(rightWristY >200 && rightWristY <= 300)
+		{
+			document.getElementById("speed").innerHTML = "Velocidade = 1.5x";		
+			song.rate(1.5);
+		}
+		else if(rightWristY >300 && rightWristY <= 400)
+		{
+			document.getElementById("speed").innerHTML = "Velocidade = 2x";		
+			song.rate(2);
+		}
+		else if(rightWristY >400)
+		{
+			document.getElementById("speed").innerHTML = "Velocidade = 2.5x";		
+			song.rate(2.5);
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+		InNumberleftWristY = Number(leftWristY); 
+		remove_decimals = floor(InNumberleftWristY);
+		volume = remove_decimals/500;
+		document.getElementById("volume").innerHTML = "Volume = " + volume;		
+		song.setVolume(volume);	
+	}
+
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
+
+	
+
+	
+
