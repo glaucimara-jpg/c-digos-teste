224ade772ff976f694348d496bdfc9be42145884
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-09-19 14:50:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,82 +1,80 @@
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-	console.log(results);
-	scoreRightWrist =  results[0].pose.keypoints[10].score;
-	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
-	
-	rightWristX = results[0].pose.rightWrist.x;
-	rightWristY = results[0].pose.rightWrist.y;
-	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-	leftWristX = results[0].pose.leftWrist.x;
-	leftWristY = results[0].pose.leftWrist.y;
-	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
-		
-  }
+song = "";
+objects = [];
+status = "";
+
+function preload()
+{
+	song = loadSound("alert.mp3");
 }
 
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
 
+function start() {
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded() {
+  console.log("Modelo Carregado!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
 
 
 function draw() {
-	image(video, 0, 0, 600, 500);
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+ 
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "person")
+          {
+            document.getElementById("numberOfObjects").innerHTML = "Bebê encontrado";
+            console.log("stop");
+            song.stop();
+          }
+          else
+          {
+            document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado";
+            console.log("play"); 
+            song.play();
+          }
+         }
 
-	fill("#FF0000");
-	stroke("#FF0000");
-
-	if(scoreRightWrist > 0.2)
-	{ 
-		circle(rightWristX,rightWristY,20);
-
-		if(rightWristY >0 && rightWristY <= 100)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 0.5x";		
-			song.rate(0.5);
-		}
-		else if(rightWristY >100 && rightWristY <= 200)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 1x";		
-			song.rate(1);
-		}
-		else if(rightWristY >200 && rightWristY <= 300)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 1.5x";		
-			song.rate(1.5);
-		}
-		else if(rightWristY >300 && rightWristY <= 400)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 2x";		
-			song.rate(2);
-		}
-		else if(rightWristY >400)
-		{
-			document.getElementById("speed").innerHTML = "Velocidade = 2.5x";		
-			song.rate(2.5);
-		}
-	}
-
-	if(scoreLeftWrist > 0.2)
-	{
-		circle(leftWristX,leftWristY,20);
-		InNumberleftWristY = Number(leftWristY); 
-		remove_decimals = floor(InNumberleftWristY);
-		volume = remove_decimals/500;
-		document.getElementById("volume").innerHTML = "Volume = " + volume;		
-		song.setVolume(volume);	
-	}
-
+        if(objects.length == 0)
+        {
+          document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado";
+          console.log("play"); 
+          song.play();
+        }
+      }
 }
-
-function play()
-{
-	song.play();
-	song.setVolume(1);
-	song.rate(1);
-}
-
 	
 
 	
